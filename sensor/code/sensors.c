8a2d6eec2460c3d02a612ed003243862f47aae41
Marcus Nguyen <marcusnguyen13@outlook.com>
2024-10-06 22:30:23 +1000

    Created sensors lib

---
+++ sensor/code/sensors.c
@@ -0,0 +1,12 @@
+// Created 06/10/2024
+
+#define BME680_ADDR 0x76 // Note the LSB is determined by the VDO state
+#define LTR329_ADDR 0x29
+
+int BME680_init(void) {
+    return 0;
+}
+
+int LTR329_init(void) {
+    return 0;
+}
